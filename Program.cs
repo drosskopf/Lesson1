05b613d1f83e728917bec4747de8e0d087bf29bb
drosskopf <drosskopf@ldschurch.org>
2017-02-21 10:02:06 -0700

    Initial Checkin

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+namespace webapp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+               .UseKestrel()
+               .Configure(app =>
+               {
+                   app.Run(async (context) =>
+                    { 
+                        var person = context.Request.Path.Value.Replace("/","");
+                        await context.Response.WriteAsync($"<h1>Hello World {person}</h1>");
+                    });
+               })
+               .Build();
+            host.Run();
+        }
+    }
+}
